c41978e066b6cc38c8cd70dac46a2479c05823aa
Jan Kocbek <jan.kocbek@gmail.com>
2020-10-02 02:11:53 +0200

    Slovenian language added (#14)
    Co-authored-by: Ben Thomson <ben@abweb.com.au>

---
+++ lang/sl/lang.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'plugin' => [
+        'name' => 'Dokumentacija',
+        'description' => 'Paket dokumentacije za October CMS',
+    ],
+    'titles' => [
+        'documentation' => 'Dokumentacija',
+        'installDocs' => 'Naloži dokumentacijo',
+        'updateDocs' => 'Osveži dokumentacijo',
+    ],
+    'links' => [
+        'docsLink' => 'Beri dokumentacijo',
+    ],
+    'buttons' => [
+        'updateDocs' => 'Osveži dokumentacijo',
+        'copyCode' => 'Kopiraj kodo',
+    ],
+    'updates' => [
+        'downloading' => 'Prenašanje dokumentacije iz repozitorija...',
+        'extracting' => 'Razširjanje dokumentacije...',
+        'rendering' => 'Upodabljanje dokumentacije...',
+        'finalizing' => 'Dokončevanje...',
+        'success' => 'Dokumentacija je bila uspešno posodobljena.',
+    ],
+    'content' => [
+        'intro' => 'Dobrodošli v dokumentaciji za October CMS. Kliknite eno od povezav na levi, da dobite več informacij o uporabi October CMS in bogastvu njegovih funkcij. Če želite dobiti najnovejšo dokumentacijo, uporabite spodnji gumb.',
+    ],
+];
