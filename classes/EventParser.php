2562c10bed416c6a8e0c5584e3bfb93e58d7181c
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-25 22:41:28 -0400

    - improve code readability - add @param parsing

--- classes/EventParser.php
+++ classes/EventParser.php
@@ -19,19 +19,19 @@ class EventParser
     {
         $data = file_get_contents($file->getPathName());
 
-        if (!preg_match_all('| +/\*\*\s+\* @event.+\*/|Us', $data, $match)) {
+        // find all event docblocks in that file
+        if (!preg_match_all('| +/\*\*\s+\* @event.+\*/|Us', $data, $matches)) {
             return;
         }
 
         $segments = explode('/', $prefix . $file->getRelativePathName());
         $trigger = implode('\\', array_map('ucfirst', $segments));
 
-        foreach ($match[0] as $doc) {
-            if (!preg_match('|@event (.+?)$|m', $doc, $match)) {
+        foreach ($matches[0] as $doc) {
+            // extract the event name
+            if (!$eventName = static::getEventName($doc)) {
                 continue;
             }
-            $eventName = $match[1];
-            $description = static::getEventDescription($doc);
 
             $event = [
                 'triggeredIn' => $trigger,
@@ -39,18 +39,57 @@ class EventParser
                 'description' => static::getEventDescription($doc),
             ];
 
-            if (preg_match('|@since (.+?)$|m', $doc, $match)) {
-                $event['since'] = $match[1];
+            if ($params = static::getParamTag($doc)) {
+                $event['params'] = $params;
+            }
+
+            if ($since = static::getSinceTag($doc)) {
+                $event['since'] = $since;
             }
 
             array_set($events, $eventName, $event);
         }
     }
 
+    protected static function getParamTag($doc)
+    {
+        $result = [];
+
+        if (preg_match_all('|@param (.+?)$|m', $doc, $match)) {
+            foreach ($match[1] as $param) {
+                $result[] = $param;
+            }
+        }
+
+        return $result;
+    }
+
+    protected static function getSinceTag($doc)
+    {
+        $result = null;
+
+        if (preg_match('|@since (.+?)$|m', $doc, $match)) {
+            $result = $match[1];
+        }
+
+        return $result;
+    }
+
+    protected static function getEventName($doc)
+    {
+        $result = null;
+
+        if (preg_match('|@event (.+?)$|m', $doc, $match)) {
+            $result = $match[1];
+        }
+
+        return $result;
+    }
+
     protected static function getEventDescription($doc)
     {
         // filter out tags, the rest is our description
-        $parts = preg_grep('/@(event|since)/', explode("\n", $doc), PREG_GREP_INVERT);
+        $parts = preg_grep('/@(event|param|since)/', explode("\n", $doc), PREG_GREP_INVERT);
 
         // filter out open/close comment lines and '*' line prefix
         $result = implode("\n", preg_filter('|^\s+?\*\s?|', '', $parts));
