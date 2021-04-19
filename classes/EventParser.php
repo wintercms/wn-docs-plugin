f7ef6cd0d0504928b4980902e714258e60646a9a
Marc Jauvin <marc.jauvin@gmail.com>
2021-04-19 10:37:23 -0400

    Add event docblock extractor/parser (#2)

---
+++ classes/EventParser.php
@@ -0,0 +1,113 @@
+<?php namespace Winter\Docs\Classes;
+
+use File;
+
+class EventParser
+{
+    public static function getPathEvents($path, $prefix = null)
+    {
+        $events = [];
+
+        foreach (File::allFiles($path) as $file) {
+            if (ends_with($file->getFilename(), '.php')) {
+                static::getFileEvents($events, $file, $prefix);
+            }
+        }
+
+        return $events;
+    }
+
+    public static function getFileEvents(&$events, $file, $prefix = null)
+    {
+        $data = file_get_contents($file->getPathName());
+
+        // find all event docblocks in that file
+        if (!preg_match_all('/ +?\/\*\*\s+?\* @event.+?\*\//s', $data, $matches, PREG_OFFSET_CAPTURE)) {
+            return;
+        }
+
+        $segments = explode('/', $prefix . $file->getRelativePathName());
+        $trigger = implode('\\', array_map('ucfirst', $segments));
+
+        $classPath = 'winter/tree/develop/modules/';
+        if ($prefix === 'winter/storm/') {
+            $classPath = 'storm/tree/develop/src/';
+        }
+        $classPath .= $file->getRelativePathName();
+
+        foreach ($matches[0] as $match) {
+            $doc = $match[0];
+            $offset = $match[1];
+            $startLine = substr_count(substr($data, 0, $offset), "\n") + 1;
+            $endLine = $startLine + substr_count($doc, "\n");
+
+            // extract the event name
+            if (!$eventName = static::getEventTag($doc)) {
+                continue;
+            }
+
+            $event = [
+                'triggeredIn' => $trigger,
+                'classPath' => $classPath . '#L' . $startLine . ',L' . ($endLine),
+                'eventName' => $eventName,
+                'description' => static::getEventDescription($doc),
+            ];
+
+            if ($params = static::getParamTag($doc)) {
+                $event['params'] = $params;
+            }
+
+            if ($since = static::getSinceTag($doc)) {
+                $event['since'] = $since;
+            }
+
+            array_set($events, $eventName, $event);
+        }
+    }
+
+    public static function getEventDescription($doc)
+    {
+        // filter out opening/closing comment and tag names
+        $result = preg_filter(['/^\s*?\/\*\*\s*?$/m', '/\s*\*\/$/s', '/@(event|since) .+$/m', '/@param [^@]+/s'], '', $doc);
+
+        // filter out spaces and asterisk prefix
+        $result = preg_filter(['/\s+?\*\s*?$/m', '/^ +?\* /m'], ["\n", ''], $result);
+
+        return trim($result);
+    }
+
+    public static function getEventTag($doc)
+    {
+        $result = null;
+
+        if (preg_match('/@event (.+?)$/m', $doc, $match)) {
+            $result = $match[1];
+        }
+
+        return $result;
+    }
+
+    public static function getParamTag($doc)
+    {
+        $result = [];
+
+        if (preg_match_all('/@param ([^@]+)/s', $doc, $matches)) {
+            foreach ($matches[1] as $match) {
+                $result[] = trim(preg_filter('/[\t ]+?\*(\s|\/)/', '', $match));
+            }
+        }
+
+        return $result;
+    }
+
+    public static function getSinceTag($doc)
+    {
+        $result = null;
+
+        if (preg_match('/@since (.+?)$/m', $doc, $match)) {
+            $result = $match[1];
+        }
+
+        return $result;
+    }
+}
---
+++ tests/cases/classes/EventParserTest.php
@@ -0,0 +1,51 @@
+<?php namespace Winter\Docs\Tests\Classes;
+
+use TestCase;
+use Winter\Docs\Classes\EventParser;
+
+class EventParserTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->eventDocBlock = file_get_contents(dirname(dirname(__DIR__)) . '/fixtures/event-doc-block.txt');
+    }
+
+    public function testGetEventDescription()
+    {
+        $this->assertEquals(
+            "Determines file changes between the specified build and the previous build.\n\nWill return an array of added, modified and removed files.",
+            EventParser::getEventDescription($this->eventDocBlock)
+        );
+    }
+
+    public function testGetEventTag()
+    {
+        $this->assertEquals(
+            'my.test.event',
+            EventParser::getEventTag($this->eventDocBlock)
+        );
+    }
+
+    public function testGetSinceTag()
+    {
+        $this->assertEquals(
+            'v1.1.2',
+            EventParser::getSinceTag($this->eventDocBlock)
+        );
+    }
+
+    public function testGetParamTag()
+    {
+        $params = EventParser::getParamTag($this->eventDocBlock);
+
+        $this->assertEquals(
+            'boolean $myParam This is my first param.',
+            $params[0]
+        );
+        $this->assertEquals(
+            "string|integer \$previous This is my second multi-line param,\n it stretches on two lines.",
+            $params[1]
+        );
+    }
+}
