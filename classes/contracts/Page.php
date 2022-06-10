0d904bbe0517d1a43b424aa1694dc3b60fa2993e
Ben Thomson <git@alfreido.com>
2022-06-10 16:31:21 +0800

    Add initial page interface method

--- classes/contracts/Page.php
+++ classes/contracts/Page.php
@@ -10,5 +10,21 @@
  */
 interface Page
 {
+    /**
+     * Gets the title of the page.
+     */
+    public function getTitle(): string;
 
+    /**
+     * Gets a navigation list for the purpose of displaying a table of contents.
+     *
+     * Navigation lists can be unlimited levels deep. Each item should have a title, and contain a `title` attribute,
+     * and either an anchor (for a linked section of the content), and/or `children` (for a section with sub-sections).
+     */
+    public function getNavigation(): array;
+
+    /**
+     * Gets the content of the page. This should generally be the rendered HTML.
+     */
+    public function getContent(): string;
 }
