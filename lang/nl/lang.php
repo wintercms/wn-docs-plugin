7bd9c32a034b1df7905d96b32c4b2fbaca54e371
ben-nl <bzweekhorst@gmail.com>
2022-08-02 07:02:04 +0200

    Add Dutch translation (#14)
    Credit to @ben-nl

---
+++ lang/nl/lang.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'plugin' => [
+        'name' => 'Docs',
+        'description' => 'Documentatie pakket voor Winter CMS.'
+    ],
+    'titles' => [
+        'documentation' => 'Documentatie',
+        'installDocs' => 'Documentatie installeren',
+        'updateDocs' => 'Documentatie vernieuwen',
+    ],
+    'links' => [
+        'docsLink' => 'Lees de documentatie',
+    ],
+    'buttons' => [
+        'updateDocs' => 'Documentatie vernieuwen',
+        'copyCode' => 'Code kopiëren',
+    ],
+    'updates' => [
+        'downloading' => 'Documentatie downloaden van repository...',
+        'extracting' => 'Documentatie uitpakken...',
+        'rendering' => 'Documentatie weergeven...',
+        'finalizing' => 'Afronden...',
+        'success' => 'De documentatie is succesvol bijgewerkt.',
+    ],
+    'content' => [
+        'intro' => 'Welkom bij de documentatie voor Winter CMS. Klik op één van de links aan de linkerkant om meer informatie
+            over het gebruik en de vele functies van Winter CMS te krijgen. Als je de meest actuele documentatie wilt ontvangen,
+            gebruik dan de onderstaande knop.'
+    ]
+];
